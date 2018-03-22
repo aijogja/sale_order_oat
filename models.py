08b808a9687b3aea800a04d7df4f5993b204e18b
Ai Jogja <cah7x.jogja@gmail.com>
2018-03-22 17:11:33 +0700

    oat on contract

--- models.py
+++ models.py
@@ -3,6 +3,12 @@
 from openerp import models, fields, api
 
 
+class AccountAnalyticAccount(models.Model):
+    _inherit = 'account.analytic.account'
+
+    oat = fields.Float(string='OAT (%)')
+
+
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
@@ -12,6 +18,29 @@ class SaleOrderLine(models.Model):
         ret_val = super(SaleOrderLine, self)._prepare_order_line_invoice_line(cr, uid, line, account_id=False, context=None)
         if not line.invoiced:
             ret_val['oat'] = line.oat
+        return ret_val
+
+    def product_id_change(self, cr, uid, ids, pricelist, product, qty=0,
+            uom=False, qty_uos=0, uos=False, name='', partner_id=False,
+            lang=False, update_tax=True, date_order=False, packaging=False,
+            fiscal_position=False, flag=False, context=None):
+        ret_val = super(SaleOrderLine, self).product_id_change(
+            cr, uid, ids, pricelist, product, qty=qty,
+            uom=uom, qty_uos=qty_uos, uos=uos, name=name, partner_id=partner_id,
+            lang=lang, update_tax=update_tax, date_order=date_order, packaging=packaging,
+            fiscal_position=fiscal_position, flag=flag, context=context)
+        if partner_id:
+            contract_obj = self.pool.get('account.analytic.account')
+            contract_ids = contract_obj.search(cr, uid,
+                [
+                    ['type', '=', 'contract'],
+                    ['state', '=', 'open'],
+                    ['partner_id', '=', partner_id]
+                ],
+                context=context
+            )
+            contract = contract_obj.browse(cr, uid, contract_ids)
+        ret_val['value']['oat'] = contract.oat
         return ret_val
 
 
