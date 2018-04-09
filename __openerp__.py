0d13c7a26dd8ee095a76ead83e3831210f60ad58
permanaloremIpsum <dpermana350@gmail.com>
2018-04-10 06:53:32 +0700

    'fix_conflict'

--- __openerp__.py
+++ __openerp__.py
@@ -18,7 +18,7 @@
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
     'category': 'Sales',
-    'version': '0.4',
+    'version': '0.5',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'sale', 'account_analytic_analysis'],
